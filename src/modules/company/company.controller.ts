7749bb316542530a34c8c8fb296d135ec08d2f1e
palash2397 <palashpatil55@gmail.com>
2026-07-29 15:09:16 +0530

    Fix: Resolve TypeScript error for req.user in company controller

--- src/modules/company/company.controller.ts
+++ src/modules/company/company.controller.ts
@@ -2,15 +2,24 @@ import {
   Body,
   Controller,
   Post,
+  Get,
+  Req,
   UploadedFiles,
+  UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
 import { CompanyService } from './company.service';
-import { ApiConsumes, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiConsumes, ApiTags } from '@nestjs/swagger';
 import { RegisterCompanyDto } from './dto/register-company.dto';
 import { LoginCompanyDto } from './dto/login-company.dto';
 import { FilesInterceptor } from '@nestjs/platform-express';
 import { multerConfig } from 'src/common/middlewares/multer';
+
+import { JwtAuthGuard } from '../auth/jwt/jwt-auth.guard';
+
+import { RoleGuard } from '../auth/roles/roles.guard';
+import { Roles } from '../auth/roles/roles.decorator';
+import { UserRole } from 'src/common/enums/user/role.enum';
 
 @ApiTags('Company')
 @Controller('company')
@@ -30,5 +39,13 @@ export class CompanyController {
   @Post('/login')
   login(@Body() dto: LoginCompanyDto) {
     return this.companyService.login(dto);
+  }
+
+  @Get('/my')
+  @ApiBearerAuth('access-token')
+  @Roles(UserRole.ADMIN)
+  @UseGuards(JwtAuthGuard, RoleGuard)
+  myProfile(@Req() req: any) {
+    return this.companyService.myProfile(req.user?.id);
   }
 }
