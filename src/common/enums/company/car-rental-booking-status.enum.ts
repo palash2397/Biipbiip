46ec9be5ca9c2f96c1adf76fd41fb9a325b45817
palash2397 <palashpatil55@gmail.com>
2026-08-19 11:29:19 +0530

    company enum

---
+++ src/common/enums/company/car-rental-booking-status.enum.ts
@@ -0,0 +1,7 @@
+export enum CarRentalBookingStatus {
+  PENDING = 'PENDING',
+  ACCEPTED = 'ACCEPTED',
+  REJECTED = 'REJECTED',
+  CANCELLED = 'CANCELLED',
+  COMPLETED = 'COMPLETED',
+}
--- src/modules/car-rental/car-rental.module.ts
+++ src/modules/car-rental/car-rental.module.ts
@@ -2,7 +2,20 @@ import { Module } from '@nestjs/common';
 import { CarRentalService } from './car-rental.service';
 import { CarRentalController } from './car-rental.controller';
 
+import { MongooseModule } from '@nestjs/mongoose';
+import {
+  CompanyCar,
+  CompanyCarSchema,
+} from '../company/schema/company-car.schema';
+import { Company, CompanySchema } from '../company/schema/company.schema';
+
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: CompanyCar.name, schema: CompanyCarSchema },
+      { name: Company.name, schema: CompanySchema },
+    ]),
+  ],
   controllers: [CarRentalController],
   providers: [CarRentalService],
 })
